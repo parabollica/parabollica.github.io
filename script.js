12a7a2ae9524590458b47eb069a8ca2e95a57cda
Parabollica <parabollicasolutions@gmail.com>
2024-01-22 18:08:26 +0200

    Updated title, added navbar link animations,  added back to top arrow button

--- script.js
+++ script.js
@@ -39,8 +39,72 @@ document.addEventListener('scroll', function () {
     handleServiceItemScroll();
 });
 
+  // Smooth scrolling for anchor links
+  document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('a.nav-link');
+    
+    navLinks.forEach(link => {
+      link.addEventListener('click', (event) => {
+        if (link.hash !== '') {
+          event.preventDefault();
+          const hash = link.hash;
+          const target = document.querySelector(hash);
+          const targetOffsetTop = target.offsetTop;
+          const scrollOptions = {
+            top: targetOffsetTop - 100,
+            behavior: 'smooth'
+          };
+          window.scrollTo(scrollOptions);
+        }
+      });
+    });
+  });
+
+//Scroll to top animation
 document.addEventListener('DOMContentLoaded', () => {
     // JavaScript to set the current year dynamically
     const currentYear = new Date().getFullYear();
     document.getElementById('current-year').textContent = currentYear;
   });  
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const backToTopButton = document.getElementById('back-to-top');
+    
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 300) {
+        backToTopButton.classList.add('active');
+      } else {
+        backToTopButton.classList.remove('active');
+      }
+    });
+    
+    backToTopButton.addEventListener('click', () => {
+      scrollToTop();
+    });
+    
+    function scrollToTop() {
+      const startPosition = window.scrollY;
+      const targetPosition = 0;
+      const startTime = performance.now();
+      const duration = 500; // Adjust the animation duration as needed
+    
+      function animateScroll(currentTime) {
+        const elapsedTime = currentTime - startTime;
+        if (elapsedTime < duration) {
+          const progress = elapsedTime / duration;
+          const easeProgress = easeOutCubic(progress);
+          const newPosition = startPosition - (startPosition * easeProgress);
+          window.scrollTo(0, newPosition);
+          requestAnimationFrame(animateScroll);
+        } else {
+          window.scrollTo(0, targetPosition);
+        }
+      }
+    
+      function easeOutCubic(t) {
+        return (t -= 1) * t * t + 1;
+      }
+    
+      requestAnimationFrame(animateScroll);
+    }
+  });
